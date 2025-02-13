e343fa904aa21cf933c2c836e0c9ba57f6bfcef5
Eduardo Savian <eduardo.savian@trust.group>
2025-02-13 15:00:41 -0300

    Add first graph

---
+++ src/graphs.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Leia os dados do arquivo
+data = pd.read_csv('mlsb.txt')
+
+# Plote os dados
+plt.figure(figsize=(10, 6))
+for key, grp in data.groupby(['Replicacao']):
+    plt.plot(grp['Iteracoes'], grp['Valor'], label=f'Replicacao {key}')
+
+plt.xlabel('Iteracoes')
+plt.ylabel('Valor')
+plt.title('Iteracoes vs Valor for different Replicacoes')
+plt.legend()
+plt.grid(True)
+plt.show()
